1cc3dfcdfe70d8ab40150ffe1b8c020fc2d1cb04
scott <qvis.be@gmail.com>
2020-05-28 11:43:25 +0800

    增加一个非运行时权限的获取设备信息的方法

--- app/src/main/java/com/otaserver/android/util/DeviceInfoUtil.java
+++ app/src/main/java/com/otaserver/android/util/DeviceInfoUtil.java
@@ -1,6 +1,9 @@
 package com.otaserver.android.util;
 
+import android.content.ContentResolver;
 import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.util.Log;
 
 import com.otaserver.android.dao.DeviceInfo;
 
@@ -19,6 +22,8 @@ public abstract class DeviceInfoUtil {
 
     Field[] fields = DeviceInfo.class.getDeclaredFields();
 
+    private static final String TAG = "DeviceInfoUtil";
+
     /**
      * 使用反射机制动态调用dto的set方法
      *
@@ -35,6 +40,31 @@ public abstract class DeviceInfoUtil {
                 break;
             }
         }
+    }
+
+    /**
+     * 不需要运行时权限获取android属性的例子，
+     * 可以获得androidID，appInstallDate，appInstallGuid三个属性。
+     */
+    public DeviceInfo getDeviceInfoNoNeedPermission(ContentResolver contentResolver) {
+
+        /**
+         * 在设备首次启动时，系统会随机生成一个64位的数字，并把这个数字以16进制字符串的形式保存下来，这个16进制的字符串就是ANDROID_ID，当设备被wipe后该值会被重置。
+         * ANDROID_ID可以作为设备标识，但需要注意：
+         * 1. 厂商定制系统的Bug：不同的设备可能会产生相同的ANDROID_ID：9774d56d682e549c。
+         * 2. 厂商定制系统的Bug：有些设备返回的值为null。
+         * 3. 设备差异：对于CDMA设备，ANDROID_ID和TelephonyManager.getDeviceId() 返回相同的值。
+         */
+        String androidId = Settings.System.getString(contentResolver, Settings.System.ANDROID_ID);
+        Log.d(TAG, "ANDROID_ID:" + androidId);
+
+        String serial = android.os.Build.SERIAL;
+        Log.d(TAG, "serial:" + serial);
+
+        DeviceInfo deviceInfo = new DeviceInfo();
+        deviceInfo.setAndroidId(androidId);
+        deviceInfo.setSerial(serial);
+        return deviceInfo;
     }
 
 }
